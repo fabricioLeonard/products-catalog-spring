04b2a249f95419e4efedf0cb268330d2c851968a
Fabricio Leonard Leopoldino <fabricioleonard@gmail.com>
2021-08-09 23:03:13 -0300

    Backend project Products Catalog created

---
+++ backend/src/main/java/br/com/developeracademy/dscatalog/ProductsCatalogApplication.java
@@ -0,0 +1,13 @@
+package br.com.developeracademy.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsCatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/developeracademy/dscatalog/ProductsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.developeracademy.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
